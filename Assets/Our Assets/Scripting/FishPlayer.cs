fa22af41ad1bb423515f95ded64b2af07f507529
Lucas <csliw30@gmail.com>
2023-07-07 12:52:39 -0700

    Fishe Controller

--- Assets/Our Assets/Scripting/FishPlayer.cs
+++ Assets/Our Assets/Scripting/FishPlayer.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FishPlayer : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+{   
+    [SerializeField]
+    private float movementSpeed;
+    [SerializeField]
+    private float turnStrength;
+    [SerializeField]
+    private Rigidbody2D body;
+
+    private Vector2 direction;
+
+    void Awake(){
+        // body = this.gameObject.GetComponent<Rigidbody2D>();
+        // if(body == null){
+        //     Debug.LogError("No RigidBody2D on player");
+        // }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate(){
+        if(body){
+            body.AddTorque(direction.x*turnStrength, ForceMode2D.Force);
+            body.AddForce(direction.y*movementSpeed*transform.up, ForceMode2D.Force);
+        }
     }
+
+    public void PlayerMove(InputAction.CallbackContext context){
+        // Debug.Log("MOVING");
+        if(body == null){
+            Debug.LogError("No RigidBody2D on player");
+        }
+        direction = context.ReadValue<Vector2>();
+    } 
 }
